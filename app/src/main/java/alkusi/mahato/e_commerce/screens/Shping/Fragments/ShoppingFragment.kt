a7afed3eebc5ad60ba6ae27552f176f4d42aa3df
Rabindra <mohnatarabindra140@gmail.com>
2024-01-31 00:32:32 +0530

    shopping layout start

---
+++ app/src/main/java/alkusi/mahato/e_commerce/screens/Shping/Fragments/ShoppingFragment.kt
@@ -0,0 +1,11 @@
+package alkusi.mahato.e_commerce.screens.Shping.Fragments
+import alkusi.mahato.e_commerce.R
+import alkusi.mahato.e_commerce.databinding.FragmentShoppingBinding
+import alkusi.mahato.e_commerce.library.views.BaseFragment
+
+
+class ShoppingFragment : BaseFragment<FragmentShoppingBinding>(R.layout.fragment_shopping) {
+    override fun FragmentShoppingBinding.initialize() {
+
+    }
+}
